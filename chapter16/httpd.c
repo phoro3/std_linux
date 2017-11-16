2065111e4439b8730857695edc3b7ec42da52f18
phoro3 <puhro20@gmail.com>
2017-11-16 23:01:22 +0900

    Implement signal handling function (page.367)

--- chapter16/httpd.c
+++ chapter16/httpd.c
@@ -14,6 +14,10 @@
 
 static void log_exit(char *fmt, ...);
 static void* xmalloc(size_t sz);
+typedef void (*sighandler_t)(int);
+static void install_signal_handlers(void);
+static void trap_signal(int sig, sighandler_t handler);
+static void signal_exit(int sig);
 
 
 int main(int argc, char *argv[])
@@ -40,4 +44,26 @@ static void* xmalloc(size_t sz)
 		log_exit("failed to allocate memory");
 	}
 	return p;
+}
+
+static void install_signal_handlers(void)
+{
+	trap_signal(SIGPIPE, signal_exit);
+}
+
+static void trap_signal(int sig, sighandler_t handler)
+{
+	struct sigaction act;
+
+	act.sa_handler = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_RESTART;
+	if (sigaction(sig, &act, NULL) < 0) {
+		log_exit("sigaction() failed: %s", strerror(errno));
+	}
+}
+
+static void signal_exit(int sig)
+{
+	log_exit("exit by signal %d", sig);
 }

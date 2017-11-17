ea8848b6b3d4ebd6cf40ce4314aff900b9ad8ac5
phoro3 <puhro20@gmail.com>
2017-11-17 22:42:39 +0900

    Implement main and service function

--- chapter16/httpd.c
+++ chapter16/httpd.c
@@ -12,16 +12,43 @@
 #include <ctype.h>
 #include <signal.h>
 
+struct HTTPHeaderField{
+	char *name;
+	char *value;
+	struct HTTPHeaderField *next;
+};
+
+struct HTTPRequest{
+	int protocol_minor_version;
+	char *method;
+	char *path;
+	struct HTTPHeaderField *header;
+	char *body;
+	long length;
+};
+
 static void log_exit(char *fmt, ...);
 static void* xmalloc(size_t sz);
 typedef void (*sighandler_t)(int);
 static void install_signal_handlers(void);
 static void trap_signal(int sig, sighandler_t handler);
 static void signal_exit(int sig);
+static void service(FILE *in, FILE *out, char *docroot);
+static struct HTTPRequest* read_request(FILE *in);
+static void respond_to(struct HTTPRequest *req, FILE *out, char *docroot);
+static void free_request(struct HTTPRequest *req);
 
 
 int main(int argc, char *argv[])
 {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+		exit(1);
+	}
+
+	install_signal_handlers();
+	service(stdin, stdout, argv[1]);
+	exit(0);
 }
 
 static void log_exit(char *fmt, ...)
@@ -66,4 +93,25 @@ static void trap_signal(int sig, sighandler_t handler)
 static void signal_exit(int sig)
 {
 	log_exit("exit by signal %d", sig);
+}
+
+static void service(FILE *in, FILE *out, char *docroot)
+{
+	struct HTTPRequest *req;
+
+	req = read_request(in);
+	respond_to(req, out, docroot);
+	free_request(req);
+}
+
+static struct HTTPRequest* read_request(FILE *in)
+{
+}
+
+static void respond_to(struct HTTPRequest *req, FILE *out, char *docroot)
+{
+}
+
+static void free_request(struct HTTPRequest *req)
+{
 }
